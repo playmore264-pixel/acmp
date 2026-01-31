98c875535ca407a7607ff5f9e1970e63211938de
playmore264-pixel <playmore264@gmail.com>
2026-01-31 13:20:19 +0500

    Update main.cpp

---
+++ 92/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c, s;
+    cin >> s;
+    c = s * 2 / 3;
+    a = b = s/6;
+    cout << a << " " << c << " " << b;
+    return 0;
+}
